dbfa97d510d93dbbfdd49fc4fbac0a57d36e077e
skdubey1983 <skd@aith.ac.in>
2026-08-24 17:49:19 +0530

    Add files via upload

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 window.STUDY_CONFIG={
   studyName:"Hidden Geometry Study",
-  submitEndpoint:"https://script.google.com/macros/s/AKfycbxl9XILQZ2woCKnMOHTjo1ikNSpM2RkwScXv7QKXdxf8cGAEa7NksQQUEcIPTeByBKHcA/exec",
+  submitEndpoint:"https://script.google.com/macros/s/AKfycbyMsUeoX2a88AI3Zin6Yh7Jqz86AJ6SlZORkUKmySUcvZRv8RrNj7IznupVsmcsJKn0rA/exec",
   requireAssignedLink:true,
   totalTasks:6,
-  maxMarksPerChannel:12
+  maxMarksPerChannel:12,maxDistinctTransitions:12
 };
