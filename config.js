93a1c272707d4db430d006d6661adde7700c3cc7
skdubey1983 <skd@aith.ac.in>
2026-08-24 17:51:05 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 window.STUDY_CONFIG={
   studyName:"Hidden Geometry Study",
-  submitEndpoint:"https://script.google.com/macros/s/AKfycbyMsUeoX2a88AI3Zin6Yh7Jqz86AJ6SlZORkUKmySUcvZRv8RrNj7IznupVsmcsJKn0rA/exec",
+  submitEndpoint:"https://script.google.com/macros/s/AKfycbwoHyspwBTdD-bjXHca9KVO0izpUnhNV7NfAqgfMDK2LJzGl0Hocm1BNdiFE-DN1KGkWQ/exec",
   requireAssignedLink:true,
   totalTasks:6,
   maxMarksPerChannel:12,maxDistinctTransitions:12
